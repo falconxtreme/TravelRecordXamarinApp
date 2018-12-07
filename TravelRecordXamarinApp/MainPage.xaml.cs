ae11f3a54667828ed98df9fffcdf06b4406aaa22
falconxtreme <falcon.donovan@gmail.com>
2018-12-06 23:45:36 -0500

    Initial check-in of module TravelRecordXamarinApp

---
+++ TravelRecordXamarinApp/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TravelRecordXamarinApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void LoginButton_Clicked(object sender, System.EventArgs e)
+        {
+            bool isEmailEmpty = string.IsNullOrEmpty(emailEntry.Text);
+            bool isPasswordEmpty = string.IsNullOrEmpty(passwordEntry.Text);
+
+            if (isEmailEmpty || isPasswordEmpty)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+    }
+
+}
