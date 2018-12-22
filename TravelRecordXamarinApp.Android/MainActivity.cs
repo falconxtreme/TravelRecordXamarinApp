622c08a9497fdc6a98d4538e04cb8e63acede1cc
falconxtreme <falcon.donovan@gmail.com>
2018-12-21 21:46:18 -0500

    * MainActivity.cs: add permissions plugin
    * Info.plist:
    * TravelRecordXamarinApp.csproj:
    * TravelRecordXamarinApp.iOS.csproj:
    * AndroidManifest.xml:
    * TravelRecordXamarinApp.Android.csproj:

--- TravelRecordXamarinApp.Android/MainActivity.cs
+++ TravelRecordXamarinApp.Android/MainActivity.cs
@@ -7,6 +7,7 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 using System.IO;
+using Plugin.Permissions;
 
 namespace TravelRecordXamarinApp.Droid
 {
@@ -22,12 +23,19 @@ namespace TravelRecordXamarinApp.Droid
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             Xamarin.FormsMaps.Init(this, savedInstanceState);
 
+            Plugin.CurrentActivity.CrossCurrentActivity.Current.Init(this, savedInstanceState);
 
             string dbName = "travel_db.sqlite";
             string folderPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
             string fullPath = Path.Combine(folderPath, dbName);
 
             LoadApplication(new App(fullPath));
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+        {
+            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
